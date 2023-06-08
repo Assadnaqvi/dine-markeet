3c9d6baca1e844354a0d07c4a54f8547924f18bd
Assad Naqvi <assadnaqvi72@gmail.com>
2023-06-08 21:55:54 +0500

    ui display full

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import Hero from './components/hero'
 import Products from './components/Products'
-
+import {client} from "./lib/sanityClient"
 import Promotions from './components/promptions'
 import UniqueJewellery from './components/uniquejewellery'
 import Subscribe from './components/subscribe'
+/*
+
+export const getProductData=async ()=>{
+  const res=await client.fetch(`*[_type=="product"]{
+  
+    title,
+    description
+  }`);
+return res
+
+
+
+}
+interface IProduct{
+
+  title:string,
+  description:string
+}
+
+
+
+
+export default async function Home(){
+  const data:IProduct[]= await getProductData()
+ console.log(data)
+  return  ( <div>
+      {data.map((item)=>(
+        <h1>{item.title}</h1>
+      ))  }
+
+    </div>
+
+  )
+}
+*/
+
 const page = () => {
   return (
     <div>
@@ -14,7 +50,8 @@ const page = () => {
     <UniqueJewellery/>
     <Subscribe/>
     </div>
-  )
+ 
+ )
 }
 
-export default page
+export default page 
---
+++ sanity/product.ts
@@ -0,0 +1,22 @@
+
+export const Product={
+    name:"product",
+    type:"document",
+    title:"Product",
+    fields:[
+        {
+            name:"title",
+            title:"Title",
+            type:"string"
+        },
+        {
+            name:"description",
+            title:"product description",
+            type:"string"
+        }, {
+            name:"image",
+            title:"product image",
+            type:"image"
+        }
+    ]
+}
